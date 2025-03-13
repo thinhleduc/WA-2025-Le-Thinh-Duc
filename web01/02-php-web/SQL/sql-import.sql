2963629d7dd8a665c7c3178e5b1ed0854c9ae555
Thinh Duc Le <thinh.duc.le@tul.cz>
2025-03-13 11:59:47 +0100

    fgg

--- web01/02-php-web/SQL/sql-import.sql
+++ web01/02-php-web/SQL/sql-import.sql
@@ -6,3 +6,9 @@ CREATE TABLE wa_test (
     user_age INT DEFAULT NULL,
     registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+INSERT INTO wa_test (user_name, user_surname, user_email, user_age) VALUES
+('Petr', 'Novák', 'petr.novak@example.com', 25),
+('Zbyšek', 'Novák', 'zbysek.novak@example.com', 27),
+('Jan', 'Jon', 'jan.jon@example.com', 21),
+('Ping', 'Pong', 'ping.pong@example.com', 25);
