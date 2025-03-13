5b4236747965366fd1a0d7a7be90a369cabe2c7f
Thinh Duc Le <thinh.duc.le@tul.cz>
2025-03-13 11:57:54 +0100

    upravy

--- web01/02-php-web/SQL/sql-import.sql
+++ web01/02-php-web/SQL/sql-import.sql
@@ -1,8 +1,8 @@
 CREATE TABLE wa_test (
-    id INT AUTO_INCREMENT PRIMARY_KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     user_name VARCHAR(50) NOT NULL,
     user_surname VARCHAR(50) NOT NULL,
     user_email VARCHAR(100) NOT NULL UNIQUE,
     user_age INT DEFAULT NULL,
     registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
+);
